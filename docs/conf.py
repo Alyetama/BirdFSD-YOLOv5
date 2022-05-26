d0bdb23d99547e8c2e9cbc344a75d120cb86fec2
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-05-26 03:25:58 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, '/Users/Felis.catus/Desktop/GitHub/bird-feeder/BirdFSD-YOLOv5/birdfsd-yolov5')
+sys.path.insert(0, f'{os.path.abspath("..")}/birdfsd-yolov5')
 
 
 # -- Project information -----------------------------------------------------
